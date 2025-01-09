06f6d5db86051d82a6697827d182f1947fd4700a
bencorpus <bencorpus7799@gmail.com>
2025-01-09 13:24:45 -0500

    P7

---
+++ scripts/Card.js
@@ -0,0 +1,37 @@
+export default class Card {
+  constructor({ name, link }, cardSelector) {
+    this._name = name;
+    this._link = link;
+    this._cardSelector = cardSelector;
+  };
+};
+
+_setEventListeners() {
+  this._cardElement.querySelector('.card__like-button').addEventListener("click", () => {
+    this._handleLikeIcon();
+  });
+
+  this._cardElement.querySelector('.card__delete-button').addEventListener("click", () => {
+    this._handleDeleteCard();
+  });
+};
+
+_handleLikeIcon() {
+  this._cardElement.querySelector('.card__like-button').classList.toggle('card__like-button-active')
+};
+
+_handleDeleteCard() {
+  this._cardElement.remove();
+  this._cardElement = null;
+};
+
+getView() {
+  this._cardElement = document
+  .querySelector(this._cardSelector)
+  .content.querySelector(".card")
+  .cardTemplate.cloneNode(true);
+  //get the card view
+  // set event listeners
+  this._setEventListeners();
+  // return the card
+}
